0e767a1880cfa509d296e98ba9540d2e27b75c60
Joel Howard <joelhoward2@gmail.com>
2022-08-30 19:05:14 -0400

    fix downcase bug

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,9 +1,10 @@
-str = "I am going down below."
+str = "Howdy partner, sit down! How's it going?"
 dict = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(str, dict)
+  str = str.downcase
   dict.reduce(Hash.new) do |hash, dict_el|
-    hash[dict_el] = str.downcase.scan(dict_el).length if str.include?(dict_el)
+    hash[dict_el] = str.scan(dict_el).length if str.include?(dict_el)
     hash
   end
 end
